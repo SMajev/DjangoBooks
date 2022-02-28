d2355817b787cb1b22d97f17355199decede9521
smajev <smajev@gmail.com>
2022-02-28 17:45:38 +0100

    Book Form

--- Books/views.py
+++ Books/views.py
@@ -11,4 +11,18 @@ class BooksView(ListView):
 class BookCreate(CreateView):
     template_name = 'book_form.html'
     form_class = BookForm
-    success_url = reverse_lazy('/')
+    success_url = reverse_lazy('books')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        cleaned_data = form.cleaned_data
+        Book.objects.create(
+            title = cleaned_data['title'],
+            author = cleaned_data['author'],
+            published_date = cleaned_data['published_date'],
+            isbn = cleaned_data['isbn'],
+            print_length = cleaned_data['print_length'],
+            cover = cleaned_data['cover'],
+            language = cleaned_data['language']
+        )
+        return result
